4d94a18ef6acfc521076242db1d108d5a97c39a3
Christian Dataro <christian@phrc.live>
2025-01-07 14:09:01 +0800

    feat: ✨  switch and if

--- projects/04-control-flow-if/src/app/app.component.ts
+++ projects/04-control-flow-if/src/app/app.component.ts
@@ -17,6 +17,20 @@ import { AccountInfo } from './account-info';
         <p>CVV: {{ account.CVV }}</p>
         <p>
           <!-- membership status -->
+          @switch (account.membershipStatus) {
+            @case ("gold") {
+              <span class="badge gold">Gold</span>
+            }
+            @case ("silver") {
+              <span class="badge silver">Silver</span>
+            }
+            @case ("platinum") {
+              <span class="badge platinum">Platinum</span>
+            }
+            @default {
+              <span class="badge platinum">Error</span>
+            }
+          }
 
           @if (account.membershipStatus === "gold") {
             <span class="badge gold">Gold</span>
